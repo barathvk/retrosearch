ed4f6a04c9821a3f89a1c76cdc17f85f559107ef
Barath Kumar <barathvk@gmail.com>
2017-02-14 03:36:48 -0600

    Switch to elastic

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 require('./lib/globals')
 const fs = require('fs')
 const scraper = require('./lib/scraper')
-const data = require('./lib/data')
 const later = require('later')
 const mkdirp = require('mkdirp')
-const search = require('./lib/search')
 const rimraf = require('rimraf')
 const api = require('./lib/api')
 const http = require('http')
@@ -14,18 +12,11 @@ const path = require('path')
 const argv = require('minimist')(process.argv.slice(2))
 const server = http.createServer(api.app)
 const bayeux = new faye.NodeAdapter({ mount: '/faye' })
-let newdir = false
 const scrape = async () => {
   await scraper.scrape(bayeux)
   logger.info('Scraping complete')
-  const list = await data.list()
-  data.list().then(all => {
-    search.all(Object.keys(all).map(a => {
-      all[a]._id = a
-      return all[a]
-    }))
-  })
-  logger.info(`Indexed ${Object.keys(list).length} roms`)
+  const c = await `http://$${config.es}/games/_count`
+  logger.info(`Indexed ${c.data.count} roms`)
 }
 if (argv.clean) rimraf.sync(config.data_dir)
 if (argv.scrape) {
@@ -39,12 +30,6 @@ if (argv.scrape) {
   logger.info('Starting indexer...')
   const schedule = later.parse.text(`every ${config.scraper.interval}`)
   later.setInterval(scrape, schedule)
-  data.list().then(all => {
-    search.all(Object.keys(all).map(a => {
-      all[a]._id = a
-      return all[a]
-    }))
-  })
   bayeux.attach(server)
   api.app.use('/', express.static(path.resolve('./public')))
   api.app.get('*', (req, res) => {
--- ui/store/index.js
+++ ui/store/index.js
@@ -1,5 +1,6 @@
 import { observable, action } from 'mobx'
 import axios from 'axios'
+import _ from 'lodash'
 export default class {
   @observable games
   @observable notification
@@ -7,16 +8,32 @@ export default class {
   pagecount = 1
   @action async load () {
     this.pagecount = 1
-    const g = await axios.get('/api?size=12')
-    this.games = g.data
+    const g = await axios.get('/api')
+    this.games = {
+      total: g.data.hits.total,
+      results: g.data.hits.hits.map(h => {
+        const hh = h._source
+        hh._id = h._id
+        return hh
+      }),
+      pages: _.chunk(_.range(0, g.data.hits.total), 12).length
+    }
     this.kw = null
     this.notify(`${this.games.total} games found`, 'success')
   }
   @action async search (kw) {
     this.pagecount = 1
-    const g = await axios.get(`/api?size=12&q=${kw}`)
+    const g = await axios.get(`/api?q=${kw}`)
     this.kw = kw
-    this.games = g.data
+    this.games = {
+      total: g.data.hits.total,
+      results: g.data.hits.hits.map(h => {
+        const hh = h._source
+        hh._id = h._id
+        return hh
+      }),
+      pages: _.chunk(_.range(0, g.data.hits.total), 12).length
+    }
   }
   @action async reload () {
     this.notify('Restarting EmulationStation', 'warning')
@@ -26,19 +43,27 @@ export default class {
   @action async loadMore () {
     this.pagecount += 1
     if (this.pagecount <= this.games.pages) {
-      let url = `/api?size=24&page=${this.pagecount}`
+      let url = `/api?page=${this.pagecount}`
       if (this.kw) {
         url += `&q=${this.kw}`
       }
       const g = await axios.get(url)
-      if (g.status === 200) g.data.results.map(r => this.games.results.push(r))
+      if (g.status === 200) {
+        g.data.hits.hits.map(r => {
+          {
+            const v = r._source
+            v._id = r._id
+            this.games.results.push(v)
+          }
+        })
+      }
     } else {
       this.pagecount -= 1
     }
   }
   @action async download (id) {
-    const meta = _.first(this.games.results.filter(g => g.id === id))
-    await axios.get(`/api/download/${id}`)
+    const meta = _.first(this.games.results.filter(g => g._id === id._id))
+    await axios.get(`/api/download/${meta.console}/${meta._id}`)
     this.notify(`Downloaded ${meta.title}`, 'success')
   }
   @action notify (message, intent, onClick) {
